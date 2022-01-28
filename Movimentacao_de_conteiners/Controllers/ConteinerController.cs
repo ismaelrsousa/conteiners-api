51e6a07efa1ba6e2927c11041fa19ec1836653f5
Ismael Rafael da Silva Sousa <ismael.silva@kbrtec.com.br>
2022-01-27 22:30:59 -0300

    CRUD Conteiners

---
+++ Movimentacao_de_conteiners/Controllers/ConteinerController.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Movimentacao_de_conteiners.Models;
+using System.Data.SqlClient;
+using System.Net;
+
+namespace Movimentacao_de_conteiners.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConteinerController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConteinerController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public List<Conteiner> Get()
+        {
+            string query = "select * from dbo.conteiner order by cd_conteiner DESC";
+
+            string sqlDataSource = _configuration.GetConnectionString("DatabaseConnection");
+
+            using (SqlConnection con = new SqlConnection(sqlDataSource))
+            {
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                con.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                List<Conteiner> conteinersList = new List<Conteiner>();
+
+                if(reader.HasRows)
+                {
+                    while(reader.Read())
+                    {
+                        Conteiner conteiner = new Conteiner(
+                            Convert.ToInt32(reader["cd_conteiner"]),
+                            Convert.ToString(reader["nm_cliente"]),
+                            Convert.ToString(reader["nr_conteiner"]),
+                            Convert.ToString(reader["nm_tipo"]),
+                            Convert.ToInt32(reader["nm_status"]),
+                            Convert.ToString(reader["nm_categoria"])
+                        );
+
+                        conteinersList.Add(conteiner);
+                    }
+                }
+
+                reader.Close();
+                con.Close();
+
+                return conteinersList;
+            }
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Post(Conteiner conteiner)
+        {
+            if (conteiner.validate())
+            {
+                string query = $"insert into dbo.conteiner values ('{conteiner.cliente}', '{conteiner.numeroConteiner}', '{conteiner.tipo}', {conteiner.status}, '{conteiner.categoria}')";
+
+                string sqlDataSource = _configuration.GetConnectionString("DatabaseConnection");
+
+                try
+                {
+                    using (SqlConnection con = new SqlConnection(sqlDataSource))
+                    {
+                        SqlCommand cmd = new SqlCommand(query, con);
+
+                        con.Open();
+
+                        SqlDataReader reader = cmd.ExecuteReader();
+
+                        reader.Close();
+                        con.Close();
+
+                        return new HttpResponseMessage(HttpStatusCode.OK);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return new HttpResponseMessage(HttpStatusCode.BadRequest);
+                }
+            } else
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpPut]
+        public HttpResponseMessage Put(Conteiner conteiner)
+        {
+            if (conteiner.validate())
+            {
+                string query = @$"update dbo.conteiner set 
+                                    nm_cliente = '{conteiner.cliente}',
+                                    nr_conteiner = '{conteiner.numeroConteiner}',
+                                    nm_tipo = '{conteiner.tipo}',
+                                    nm_status = {conteiner.status},
+                                    nm_categoria = '{conteiner.categoria}'
+                                where cd_conteiner = {conteiner.id}";
+
+                string sqlDataSource = _configuration.GetConnectionString("DatabaseConnection");
+
+                try
+                {
+                    using (SqlConnection con = new SqlConnection(sqlDataSource))
+                    {
+                        SqlCommand cmd = new SqlCommand(query, con);
+
+                        con.Open();
+
+                        SqlDataReader reader = cmd.ExecuteReader();
+
+                        reader.Close();
+                        con.Close();
+
+                        return new HttpResponseMessage(HttpStatusCode.OK);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return new HttpResponseMessage(HttpStatusCode.BadRequest);
+                }
+            } else
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public HttpResponseMessage Delete(int id)
+        {
+            string query = $"delete from dbo.conteiner where cd_conteiner = {id}";
+
+            string sqlDataSource = _configuration.GetConnectionString("DatabaseConnection");
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(sqlDataSource))
+                {
+                    SqlCommand cmd = new SqlCommand(query, con);
+
+                    con.Open();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    reader.Close();
+                    con.Close();
+
+                    return new HttpResponseMessage(HttpStatusCode.OK);
+                }
+            }
+            catch (Exception ex)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
---
+++ Movimentacao_de_conteiners/Models/Conteiner.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Movimentacao_de_conteiners.Models
+{
+    public class Conteiner
+    {
+        public Conteiner(int id, string cliente, string numeroConteiner, string tipo, int status, string categoria)
+        {
+            this.id = id;
+            this.cliente = cliente;
+            this.numeroConteiner = numeroConteiner;
+            this.tipo = tipo;
+            this.status = status;
+            this.categoria = categoria;
+        }
+
+        public int id { get; set; }
+        public string cliente { get; set; }
+        public string numeroConteiner { get; set; }
+        public string tipo { get; set; }
+        public int status { get; set; }
+        public string categoria { get; set; }
+
+        public bool validate()
+        {
+
+            if(this.cliente == "") return false;
+
+            if(!Regex.IsMatch(this.numeroConteiner, "[A-Z]{4}[0-9]{7}")) return false;
+
+            string[] tipos = { "20", "40" };
+            if(!tipos.Contains(this.tipo)) return false;
+
+            string[] categorias = { "Importação", "Exportação" };
+            if (!categorias.Contains(this.categoria)) return false;
+
+            int[] status = { 0, 1 };
+            if(!status.Contains(this.status)) return false;
+
+            return true;
+        }
+    }
+}
